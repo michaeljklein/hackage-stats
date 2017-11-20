c7d17920698bce059f3b160076ee1079fc9f38b8
Michael J Klein <michaeljklein@users.noreply.github.com>
2017-11-20 14:40:25 -0500

    Reorganize (#4)
    * move graph results files
    
    * move haskell project to root
    
    * update build docs, cabal file
    
    * remove spaces from filenames
    
    * update cabal name

--- build_docs.sh
+++ build_docs.sh
@@ -2,4 +2,4 @@
 set -ex
 read  -n 1 -p "Press enter to fresh-rebuild docs: "
 rm -f docs/build.log || echo 'No previous build log at: docs/build.log'
-(cd hackage-info && time stack build --haddock --haddock-deps --haddock-internal --haddock-arguments="-o ../docs" 2>&1 | tee -a ../docs/build.log)
+(time stack build --haddock --haddock-deps --haddock-internal --haddock-arguments="-o ../docs" 2>&1 | tee -a ../docs/build.log)
