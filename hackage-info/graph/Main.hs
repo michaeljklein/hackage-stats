67650f417f4f2f9ad9f9e6aa28ce7c2a6513ca7d
michaeljklein <michael14486@gmail.com>
2017-11-20 01:22:32 -0500

    cleanup pass

--- hackage-info/graph/Main.hs
+++ hackage-info/graph/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TupleSections #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
@@ -32,7 +33,7 @@ instance Functor WeightedEdge where
 nameMap :: [Package] -> M.Map Int String
 nameMap = foldr ($!) M.empty . map (uncurry M.insert) . zip [0..] . map _name
 
-mapSwap :: Map k a -> Map a k
+mapSwap :: Ord a => M.Map k a -> M.Map a k
 mapSwap = M.fromList . map swap . M.toAscList
 
 packageSizedEdges :: M.Map String Int -> Package -> [WeightedEdge Integer]
@@ -57,11 +58,12 @@ toDouble = fromRational . (% 1)
 nodeLabels :: [Package] -> [(String, String, String)]
 nodeLabels ps = map nodeLabel ps
   where
-    namef = (p ^. name,,)
     nodeLabel p | isNothing $ p ^. repo                     = namef "" "0.0"
                 | isNothing $ fromJust (p ^. repo) ^. size  = namef "" "0.0"
                 | (== Just 0) $ fromJust (p ^. repo) ^. size = namef "" "0.0"
                 | otherwise                                 = namef (p ^. name) $ show . toDouble . hstep . subtract 7517 . fromJust $ p ^. repo ^. size
+      where
+        namef = (p ^. name,,)
 
 
 showe :: M.Map Int String -> WeightedEdge Integer -> (String, String, String)
@@ -69,6 +71,7 @@ showe _ (Wedge x 0 y) = (show x, show . toDouble $ 0, show y)
 showe m (Wedge x w y) = (show $ m M.! x, show . toDouble $ 0, show $ m M.! y)
 
 
+main :: IO ()
 main = do
   args <- getArgs
   when (null args) $ putStrLn "Input arg should be .json file"
@@ -76,8 +79,7 @@ main = do
   let namemap = nameMap packages
   let valmap  = mapSwap namemap
   let sedges  = map top50 . sizedEdges valmap $ packages
-
-
+  (mapM_ print :: [WeightedEdge Integer] -> IO ()) sedges
 
 
 
